c4fa146bff40b136c9727860260e13af9bba0673
hedo95 <44504802+hedo95@users.noreply.github.com>
2020-05-19 02:09:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 from utilities import *
 
 path = '' # current image path
+format = '' # image format to be saved
 im = plt.imread(path)
 eq_im = ecualizaYUV(im)
+plt.imsave(path,eq_im,format=format)
 plt.imshow(eq_im)
